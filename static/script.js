4d0f01360b1fc78f9f256612ab0edad8820628eb
kyzn-15 <kevin.johan.will@gmail.com>
2024-08-23 19:42:14 +0700

    first commit

---
+++ static/script.js
@@ -0,0 +1,12 @@
+// Shown up 
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        })
+    })
+    const hiddenElements = document.querySelectorAll(".hidden");
+    hiddenElements.forEach((el) => observer.observe(el));
+})
