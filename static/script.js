bda5de2797ce3c3759730f21f0b7175480ab7536
rhasyafaqih03 <rhasyafaqihputrasuryadharma@students.ignatiusglobal.sch.id>
2024-08-30 19:30:47 +0700

    nambahin chatBox

--- static/script.js
+++ static/script.js
@@ -29,15 +29,33 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // Detail Activities
-function showDetail(eventName, classEvent) {
+function showDetail(eventName, classEvent, descEvent) {
   document.getElementById("detail").style.display = "block";
   const heading = document.getElementById("detail-heading");
   const logo = document.getElementById("detail-logo");
+  const desc = document.getElementById("detail-desc")
 
   heading.innerText = eventName;
   logo.className = classEvent;
+  desc.innerText = descEvent;
 }
 
 function closeDetail() {
   document.getElementById("detail").style.display = "none";
 }
+
+// Chat Box Mascott
+document.addEventListener("DOMContentLoaded", function () {
+  const mascot = document.getElementById("chat-mascott");
+  const chatBox = document.getElementById("chatBox");
+
+  mascot.onclick = () => {
+    chatBox.classList.toggle("show-box");
+  };
+  var typed = new Typed('#chatBox', {
+    strings: ['Rhasya kece banget anjir sigma skibidi toilet.', 'Kevin pro player epep dan pabji', 'Syahdan alpha male -999 aura'],
+    typeSpeed: 50,
+    loop : true
+  });
+
+});
