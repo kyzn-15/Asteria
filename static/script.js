42caeaa60abf6c920fc2521a407dcd377a3ea558
rhasyafaqih03 <rhasyafaqihputrasuryadharma@students.ignatiusglobal.sch.id>
2024-08-31 17:34:30 +0700

    update about, nambah fotbar

--- static/script.js
+++ static/script.js
@@ -33,7 +33,7 @@ function showDetail(eventName, classEvent, descEvent) {
   document.getElementById("detail").style.display = "block";
   const heading = document.getElementById("detail-heading");
   const logo = document.getElementById("detail-logo");
-  const desc = document.getElementById("detail-desc")
+  const desc = document.getElementById("detail-desc");
 
   heading.innerText = eventName;
   logo.className = classEvent;
@@ -52,10 +52,13 @@ document.addEventListener("DOMContentLoaded", function () {
   mascot.onclick = () => {
     chatBox.classList.toggle("show-box");
   };
-  var typed = new Typed('#chatBox', {
-    strings: ['Rhasya kece banget anjir sigma skibidi toilet.', 'Kevin pro player epep dan pabji', 'Syahdan alpha male -999 aura'],
+  var typed = new Typed("#chatBox", {
+    strings: [
+      "ASTERIA ada api unggun lohhh!!",
+      "Kapan lagi nih nginap di sekolah sama teman teman seangkatann? ",
+      "Duh aku pengen banget nontonin performance dari Jason, Yusuf, Padus dan lain lainnya..!",
+    ],
     typeSpeed: 50,
-    loop : true
+    loop: true,
   });
-
 });
